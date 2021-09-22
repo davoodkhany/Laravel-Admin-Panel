fb91c70b170513db55e6b4f599c6c99c679f9412
davood <davoodkhany@gmail.com>
2021-09-22 23:22:07 +0330

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') | Nazox - Responsive Bootstrap 4 Admin Dashboard</title>
+    <title>@yield('title') | xoopiter - Responsive Bootstrap 4 Admin Dashboard</title>
     <link rel="shortcut icon" href="{{ URL::asset('/assets/images/favicon.ico')}}">
     @include('layouts.head')
 </head>
